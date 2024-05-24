eac2ec8c19d1584a96c95f8a609d4502d0262de2
pedroricardo14 <pedroricardo1032@gmail.com>
2024-05-23 21:08:08 -0300

    anatomiaDasClasses.java -3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,7 +2,9 @@ public class MinhaClasse {
     
 
     public static void main (String [] args) {
-        System.out.print("Olá turma, sejam bem-vindos");
+       // System.out.print("Olá turma, sejam bem-vindos");
+
+      // final String BR = "Brasil";
     }
 
 
