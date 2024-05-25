b86a46f20aa65bd732e9796ef7dd0873e4791944
pedroricardo14 <pedroricardo1032@gmail.com>
2024-05-24 21:16:02 -0300

    Anatomia das classes -4

---
+++ java-anatomia-classes/src/boletimEstudantil.java
@@ -0,0 +1,14 @@
+public class boletimEstudantil {
+    public static void main(String[] args) {
+        // identação
+        int mediaFinal = 7;
+            if(mediaFinal<6)
+                System.out.println("REPROVADO");
+        else if(mediaFinal==6)
+                System.out.println("PROVA MINERVA");
+        else 
+                System.out.println("APROVADO");
+
+        // Tenho uma classe, dentro dela tenho métodos, dentro de métodos eu tenho fluxos, e dentro desse fluxo alguma ação será executada se essa correspondência for atribuida ou comtemplada
+    }
+}
