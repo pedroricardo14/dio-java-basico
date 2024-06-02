8e8a897975228c7b83a5f014a7572e302980a5d0
pedroricardo14 <pedroricardo1032@gmail.com>
2024-06-02 20:09:17 -0300

    Java Doc

---
+++ java-doc/java-doc/src/Calculadora.java
@@ -0,0 +1,33 @@
+/**
+ * <h1>Calculadora<h1/>
+ * A calculadora realiza operações matemáticas emtre números inteiros 
+ * <p/>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Pedro Ricardo
+ * @version 1.0
+ * @since 02/06/2024
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+        
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     * Que incrível !!!
+     */
+    public void metodo(){
+
+    }
+}
---
+++ java-doc/java-doc/src/Comentarios.java
@@ -0,0 +1,11 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    }
+}
