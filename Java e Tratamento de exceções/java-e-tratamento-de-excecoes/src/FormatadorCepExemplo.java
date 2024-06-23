e1715d33052dbab7451222e92523a8fb63178e91
pedroricardo14 <pedroricardo1032@gmail.com>
2024-06-23 15:01:55 -0300

    Hierarquia das exceções e exceções customizadas.

---
+++ Java e Tratamento de exceções/java-e-tratamento-de-excecoes/src/FormatadorCepExemplo.java
@@ -0,0 +1,18 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            System.out.println("O cep não corresponde com as regras de negócio.");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+            throw new CepInvalidoException();
+
+            // Simulando um cep formatado
+            return "23.765-064";
+    }
+}
